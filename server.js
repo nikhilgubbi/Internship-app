ab2b7bac6401f6337b0fae04047354fe6f0b29c4
NIKHIL GR <44517028+nikhilgubbi@users.noreply.github.com>
2021-05-19 01:28:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const pdf = require('html-pdf');
+const cors = require('cors');
+
+const markSheetTemp = require('./documents');
 
 const app = express();
+
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.post('/create-pdf', (req, res) => {
+    pdf.create(markSheetTemp(req.body), {}).toFile('certificate.pdf', (err) => {
+        if (err) {
+            res.send(Promise.reject());
+        }
+
+        res.send(Promise.resolve());
+    });
 });
-
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
-
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
-    
-  // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`))
